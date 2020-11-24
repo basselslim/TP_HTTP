8a3b3239e5b4fea423a542360375965496d8283f
Clément Parret <clement.parret@gmail.com>
2020-11-25 00:04:02 +0100

    random color

--- doc/action.js
+++ doc/action.js
@@ -1,3 +1,14 @@
-function setCouleurBackground(couleur){
-    document.body.style.backgroundColor = couleur;
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+
+
+function setRandomColor() {
+    document.body.style.backgroundColor = getRandomColor();
 }
